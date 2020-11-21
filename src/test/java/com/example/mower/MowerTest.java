a95ce731650cdc8c73de1b6377aaf6fa8d086b36
Your Name <you@example.com>
2020-11-21 11:42:04 +0100

    Introduce Nested tests for better orgnization and readability of the tests

--- src/test/java/com/example/mower/MowerTest.java
+++ src/test/java/com/example/mower/MowerTest.java
@@ -1,6 +1,7 @@
 package com.example.mower;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static com.example.mower.Command.*;
@@ -23,131 +24,141 @@ public class MowerTest {
     assertThat(mower.getPosition()).isEqualTo(expectedPosition);
   }
 
-  @Test
-  void givenMowerFacingNorth_andNotFacingLimit_whenGoForward_thenYCoordinateIncreasesBy1() {
-    Mower mower = new Mower(1, 2, N, environment);
-    Position expectedPosition = new Position(1, 3, N);
-    mower.execute(A);
-    assertThat(mower.getPosition()).isEqualTo(expectedPosition);
+  @Nested
+  class GoForward{
+    @Test
+    void givenMowerFacingNorth_andNotFacingLimit_whenGoForward_thenYCoordinateIncreasesBy1() {
+      Mower mower = new Mower(1, 2, N, environment);
+      Position expectedPosition = new Position(1, 3, N);
+      mower.execute(A);
+      assertThat(mower.getPosition()).isEqualTo(expectedPosition);
+    }
+
+    @Test
+    void givenMowerFacingNorth_andFacingLimit_whenGoForward_thenYCoordinateDoesNotChange() {
+      Mower mower = new Mower(1, 5, N, environment);
+      Position expectedPosition = new Position(1, 5, N);
+      mower.execute(A);
+      assertThat(mower.getPosition()).isEqualTo(expectedPosition);
+    }
+
+    @Test
+    void givenMowerFacingWest_whenGoForward_thenXCoordinateDecreasesBy1() {
+      Mower mower = new Mower(1, 5, W, environment);
+      Position expectedPosition = new Position(0, 5, W);
+      mower.execute(A);
+      assertThat(mower.getPosition()).isEqualTo(expectedPosition);
+    }
+
+    @Test
+    void givenMowerFacingWest_andFacingLimit_whenGoForward_thenXCoordinateDoesNotChange() {
+      Mower mower = new Mower(0, 5, W, environment);
+      Position expectedPosition = new Position(0, 5, W);
+      mower.execute(A);
+      assertThat(mower.getPosition()).isEqualTo(expectedPosition);
+    }
+
+    @Test
+    void givenMowerFacingEast_whenGoForward_thenXCoordinateDecreasesBy1() {
+      Mower mower = new Mower(1, 5, E, environment);
+      Position expectedPosition = new Position(2, 5, E);
+      mower.execute(A);
+      assertThat(mower.getPosition()).isEqualTo(expectedPosition);
+    }
+
+    @Test
+    void givenMowerFacingEast_andFacingBorder_whenGoForward_thenXCoordinateDoesNotChange() {
+      Mower mower = new Mower(5, 5, E, environment);
+      Position expectedPosition = new Position(5, 5, E);
+      mower.execute(A);
+      assertThat(mower.getPosition()).isEqualTo(expectedPosition);
+    }
+
+    @Test
+    void givenMowerFacingSouth_whenGoForward_thenYCoordinateDecreasesBy1() {
+      Mower mower = new Mower(1, 5, S, environment);
+      Position expectedPosition = new Position(1, 4, S);
+      mower.execute(A);
+      assertThat(mower.getPosition()).isEqualTo(expectedPosition);
+    }
+
+    @Test
+    void givenMowerFacingSouth_andFacingBorder_whenGoForward_thenYCoordinateDoesNotChange() {
+      Mower mower = new Mower(1, 0, S, environment);
+      Position expectedPosition = new Position(1, 0, S);
+      mower.execute(A);
+      assertThat(mower.getPosition()).isEqualTo(expectedPosition);
+    }
   }
 
-  @Test
-  void givenMowerFacingNorth_andFacingLimit_whenGoForward_thenYCoordinateDoesNotChange() {
-    Mower mower = new Mower(1, 5, N, environment);
-    Position expectedPosition = new Position(1, 5, N);
-    mower.execute(A);
-    assertThat(mower.getPosition()).isEqualTo(expectedPosition);
+  @Nested
+  class TurnLeft{
+    @Test
+    void givenMowerFacingNorth_whenTurnLeft_thenFacingWest() {
+      Mower mower = new Mower(1, 5, N, environment);
+      Position expectedPosition = new Position(1, 5, W);
+      mower.execute(G);
+      assertThat(mower.getPosition()).isEqualTo(expectedPosition);
+    }
+
+    @Test
+    void givenMowerFacingWest_whenTurnLeft_thenFacingSouth() {
+      Mower mower = new Mower(1, 5, W, environment);
+      Position expectedPosition = new Position(1, 5, S);
+      mower.execute(G);
+      assertThat(mower.getPosition()).isEqualTo(expectedPosition);
+    }
+
+    @Test
+    void givenMowerFacingSouth_whenTurnLeft_thenFacingEast() {
+      Mower mower = new Mower(1, 5, S, environment);
+      Position expectedPosition = new Position(1, 5, E);
+      mower.execute(G);
+      assertThat(mower.getPosition()).isEqualTo(expectedPosition);
+    }
+
+    @Test
+    void givenMowerFacingEast_whenTurnLeft_thenFacingNorth() {
+      Mower mower = new Mower(1, 5, E, environment);
+      Position expectedPosition = new Position(1, 5, N);
+      mower.execute(G);
+      assertThat(mower.getPosition()).isEqualTo(expectedPosition);
+    }
   }
 
-  @Test
-  void givenMowerFacingWest_whenGoForward_thenXCoordinateDecreasesBy1() {
-    Mower mower = new Mower(1, 5, W, environment);
-    Position expectedPosition = new Position(0, 5, W);
-    mower.execute(A);
-    assertThat(mower.getPosition()).isEqualTo(expectedPosition);
+  @Nested
+  class TurnRight{
+    @Test
+    void givenMowerFacingNorth_whenTurnRight_thenFacingEast() {
+      Mower mower = new Mower(1, 5, N, environment);
+      Position expectedPosition = new Position(1, 5, E);
+      mower.execute(D);
+      assertThat(mower.getPosition()).isEqualTo(expectedPosition);
+    }
+
+    @Test
+    void givenMowerFacingEast_whenTurnRight_thenFacingSouth() {
+      Mower mower = new Mower(1, 5, E, environment);
+      Position expectedPosition = new Position(1, 5, S);
+      mower.execute(D);
+      assertThat(mower.getPosition()).isEqualTo(expectedPosition);
+    }
+
+    @Test
+    void givenMowerFacingSouth_whenTurnRight_thenFacingWest() {
+      Mower mower = new Mower(1, 5, S, environment);
+      Position expectedPosition = new Position(1, 5, W);
+      mower.execute(D);
+      assertThat(mower.getPosition()).isEqualTo(expectedPosition);
+    }
+
+    @Test
+    void givenMowerFacingWest_whenTurnRight_thenFacingRight() {
+      Mower mower = new Mower(1, 5, W, environment);
+      Position expectedPosition = new Position(1, 5, N);
+      mower.execute(D);
+      assertThat(mower.getPosition()).isEqualTo(expectedPosition);
+    }
   }
 
-  @Test
-  void givenMowerFacingWest_andFacingLimit_whenGoForward_thenXCoordinateDoesNotChange() {
-    Mower mower = new Mower(0, 5, W, environment);
-    Position expectedPosition = new Position(0, 5, W);
-    mower.execute(A);
-    assertThat(mower.getPosition()).isEqualTo(expectedPosition);
-  }
-
-  @Test
-  void givenMowerFacingEast_whenGoForward_thenXCoordinateDecreasesBy1() {
-    Mower mower = new Mower(1, 5, E, environment);
-    Position expectedPosition = new Position(2, 5, E);
-    mower.execute(A);
-    assertThat(mower.getPosition()).isEqualTo(expectedPosition);
-  }
-
-  @Test
-  void givenMowerFacingEast_andFacingBorder_whenGoForward_thenXCoordinateDoesNotChange() {
-    Mower mower = new Mower(5, 5, E, environment);
-    Position expectedPosition = new Position(5, 5, E);
-    mower.execute(A);
-    assertThat(mower.getPosition()).isEqualTo(expectedPosition);
-  }
-
-  @Test
-  void givenMowerFacingSouth_whenGoForward_thenYCoordinateDecreasesBy1() {
-    Mower mower = new Mower(1, 5, S, environment);
-    Position expectedPosition = new Position(1, 4, S);
-    mower.execute(A);
-    assertThat(mower.getPosition()).isEqualTo(expectedPosition);
-  }
-
-  @Test
-  void givenMowerFacingSouth_andFacingBorder_whenGoForward_thenYCoordinateDoesNotChange() {
-    Mower mower = new Mower(1, 0, S, environment);
-    Position expectedPosition = new Position(1, 0, S);
-    mower.execute(A);
-    assertThat(mower.getPosition()).isEqualTo(expectedPosition);
-  }
-
-  @Test
-  void givenMowerFacingNorth_whenTurnLeft_thenFacingWest() {
-    Mower mower = new Mower(1, 5, N, environment);
-    Position expectedPosition = new Position(1, 5, W);
-    mower.execute(G);
-    assertThat(mower.getPosition()).isEqualTo(expectedPosition);
-  }
-
-  @Test
-  void givenMowerFacingWest_whenTurnLeft_thenFacingSouth() {
-    Mower mower = new Mower(1, 5, W, environment);
-    Position expectedPosition = new Position(1, 5, S);
-    mower.execute(G);
-    assertThat(mower.getPosition()).isEqualTo(expectedPosition);
-  }
-
-  @Test
-  void givenMowerFacingSouth_whenTurnLeft_thenFacingEast() {
-    Mower mower = new Mower(1, 5, S, environment);
-    Position expectedPosition = new Position(1, 5, E);
-    mower.execute(G);
-    assertThat(mower.getPosition()).isEqualTo(expectedPosition);
-  }
-
-  @Test
-  void givenMowerFacingEast_whenTurnLeft_thenFacingNorth() {
-    Mower mower = new Mower(1, 5, E, environment);
-    Position expectedPosition = new Position(1, 5, N);
-    mower.execute(G);
-    assertThat(mower.getPosition()).isEqualTo(expectedPosition);
-  }
-
-  @Test
-  void givenMowerFacingNorth_whenTurnRight_thenFacingEast() {
-    Mower mower = new Mower(1, 5, N, environment);
-    Position expectedPosition = new Position(1, 5, E);
-    mower.execute(D);
-    assertThat(mower.getPosition()).isEqualTo(expectedPosition);
-  }
-
-  @Test
-  void givenMowerFacingEast_whenTurnRight_thenFacingSouth() {
-    Mower mower = new Mower(1, 5, E, environment);
-    Position expectedPosition = new Position(1, 5, S);
-    mower.execute(D);
-    assertThat(mower.getPosition()).isEqualTo(expectedPosition);
-  }
-
-  @Test
-  void givenMowerFacingSouth_whenTurnRight_thenFacingWest() {
-    Mower mower = new Mower(1, 5, S, environment);
-    Position expectedPosition = new Position(1, 5, W);
-    mower.execute(D);
-    assertThat(mower.getPosition()).isEqualTo(expectedPosition);
-  }
-
-  @Test
-  void givenMowerFacingWest_whenTurnRight_thenFacingRight() {
-    Mower mower = new Mower(1, 5, W, environment);
-    Position expectedPosition = new Position(1, 5, N);
-    mower.execute(D);
-    assertThat(mower.getPosition()).isEqualTo(expectedPosition);
-  }
 }
