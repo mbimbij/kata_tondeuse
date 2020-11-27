1df97b61636f1522051008b17c7e4bef5ddcfc26
Your Name <you@example.com>
2020-11-27 20:57:48 +0100

    Duplicate tests for `MowerApplication`, but injecting a `CommandExecutor` factory instead of a `Mower` factory

--- src/main/java/com/example/mower/Main.java
+++ src/main/java/com/example/mower/Main.java
@@ -4,8 +4,9 @@ public class Main {
   public static void main(String[] args) {
     String inputFilePath = "src/main/resources/inputFile.txt";
     String outputFilePath = "src/main/resources/outputFile.txt";
-    CommandExecutorFactory mowerFactory = new MowerFactory();
-    MowerApplication mowerApplication = new MowerApplication(inputFilePath, outputFilePath, mowerFactory);
+//    CommandExecutorFactory commandExecutorFactory = new MowerFactory();
+    CommandExecutorFactory commandExecutorFactory = new CommandExecutorFactoryImpl();
+    MowerApplication mowerApplication = new MowerApplication(inputFilePath, outputFilePath, commandExecutorFactory);
     mowerApplication.run();
   }
 }
---
+++ src/test/java/com/example/mower/CommandExecutorApplicationTest.java
@@ -0,0 +1,98 @@
+package com.example.mower;
+
+import org.assertj.core.api.SoftAssertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+
+import static com.example.mower.Command.A;
+import static com.example.mower.Command.G;
+import static com.example.mower.Orientation.N;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class CommandExecutorApplicationTest {
+
+  private final String inputFilePathString = "src/test/resources/testInputFile.txt";
+  private final String outputFilePathString = "src/test/resources/testOutputFile.txt";
+  private MowerApplication mowerApplication;
+
+  @BeforeEach
+  void setUp() throws IOException {
+    clearOuputFile();
+    mowerApplication = new MowerApplication(inputFilePathString, outputFilePathString, new CommandExecutorFactoryImpl());
+  }
+
+  private void clearOuputFile() throws IOException {
+    Path outputFilePath = Paths.get(outputFilePathString);
+    if (!Files.exists(outputFilePath)) {
+      Files.createFile(outputFilePath);
+    }
+    PrintWriter pw = new PrintWriter(outputFilePathString);
+    pw.close();
+  }
+
+  @Test
+  void givenExistingFile_whenCreateMowerApplication_thenExceptionIsNotThrown() {
+    assertDoesNotThrow(() -> new MowerApplication(inputFilePathString, outputFilePathString, new MowerFactory()));
+  }
+
+  @Test
+  void givenNonExistingFile_whenCreateMowerApplication_thenExceptionIsThrown() {
+    String inputFilePathString = "src/test/resources/notExistingFile.txt";
+    assertThrows(FileNotFoundException.class, () -> new MowerApplication(inputFilePathString, outputFilePathString, new MowerFactory()));
+  }
+
+  @Test
+  void canCreateEnvironmentFromString() {
+    String environmentString = "5 6";
+    Environment environment = mowerApplication.createEnvironment(environmentString);
+    SoftAssertions.assertSoftly(softAssertions -> {
+      softAssertions.assertThat(environment.getXLimit()).isEqualTo(5);
+      softAssertions.assertThat(environment.getYLimit()).isEqualTo(6);
+    });
+  }
+
+  @Test
+  void canCreateMowerFromString() {
+    String mowerString = "1 2 N";
+    IExecuteCommands mower = new MowerFactory().createCommandExecutor(mowerString, null);
+    Position expectedInitialPosition = new MutablePosition(1, 2, N);
+    assertThat(mower.getPosition()).isEqualTo(expectedInitialPosition);
+  }
+
+  @Test
+  void canCreateCommandsFromString() {
+    // GIVEN
+    String commandsString = "GAGAGAGAA";
+
+    // WHEN
+    List<Command> commands = mowerApplication.createInstructions(commandsString);
+
+    // THEN
+    assertThat(commands).containsExactly(G, A, G, A, G, A, G, A, A);
+  }
+
+  @Test
+  void givenTestInputFile_whenRun_thenOutputFileHasExpectedContent() throws IOException {
+    // WHEN
+    mowerApplication.run();
+
+    // THEN
+    List<String> expectedOutputFileContent = Arrays.asList(
+        "1 3 N",
+        "5 1 E");
+    Path outputFilePath = Paths.get(outputFilePathString);
+    List<String> outputFileContent = Files.readAllLines(outputFilePath);
+    assertThat(outputFileContent).containsExactlyElementsOf(expectedOutputFileContent);
+  }
+}
