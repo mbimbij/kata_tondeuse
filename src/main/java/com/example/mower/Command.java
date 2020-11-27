0a3b1235b6e20cc8e3387025449a72d7ab723955
Your Name <you@example.com>
2020-11-27 09:06:03 +0100

    Move  methods to the Position class

--- src/main/java/com/example/mower/Command.java
+++ src/main/java/com/example/mower/Command.java
@@ -1,7 +1,5 @@
 package com.example.mower;
 
-import static com.example.mower.Orientation.*;
-
 public enum Command {
   A {
     @Override
@@ -10,66 +8,51 @@ public enum Command {
     }
 
     private ImmutablePosition goForward(Position position, Environment environment) {
-      if (isFacingNorth(position) && !isFacingNorthBorder(position, environment)) {
-        return goForwardUp(position);
-      } else if (isFacingWest(position) && !isFacingWestBorder(position)) {
-        return goForwardLeft(position);
-      } else if (isFacingEast(position) && !isFacingEastBorder(position, environment)) {
-        return goForwardRight(position);
-      } else if (isFacingSouth(position) && !isFacingSouthBorder(position, environment)) {
-        return goForwardDown(position);
+      if (position.isFacingNorth() && !isFacingNorthBorder(position, environment)) {
+        return goUp(position);
+      } else if (position.isFacingWest() && !isFacingWestBorder(position)) {
+        return goLeft(position);
+      } else if (position.isFacingEast() && !isFacingEastBorder(position, environment)) {
+        return goRight(position);
+      } else if (position.isFacingSouth() && !isFacingSouthBorder(position)) {
+        return goDown(position);
       } else {
         return new ImmutablePosition(position.getX(), position.getY(), position.getOrientation());
       }
     }
 
-    private ImmutablePosition goForwardDown(Position position) {
+    private ImmutablePosition goDown(Position position) {
       return new ImmutablePosition(position.getX(), position.getY() - 1, position.getOrientation());
     }
 
-    private boolean isFacingSouthBorder(Position position, Environment environment) {
-      return position.getY() <= 0;
-    }
-
-    private boolean isFacingSouth(Position position) {
-      return position.getOrientation() == S;
-    }
-
-    private ImmutablePosition goForwardRight(Position position) {
+    private ImmutablePosition goRight(Position position) {
       return new ImmutablePosition(position.getX() + 1, position.getY(), position.getOrientation());
+    }
+
+    private ImmutablePosition goLeft(Position position) {
+      return new ImmutablePosition(position.getX() - 1, position.getY(), position.getOrientation());
+    }
+
+    private ImmutablePosition goUp(Position position) {
+      return new ImmutablePosition(position.getX(), position.getY() + 1, position.getOrientation());
+    }
+
+    private boolean isFacingSouthBorder(Position position) {
+      return position.getY() <= 0;
     }
 
     private boolean isFacingEastBorder(Position position, Environment environment) {
       return position.getX() >= environment.getXLimit();
     }
 
-    private boolean isFacingEast(Position position) {
-      return position.getOrientation() == E;
-    }
-
-    private ImmutablePosition goForwardLeft(Position position) {
-      return new ImmutablePosition(position.getX() - 1, position.getY(), position.getOrientation());
-    }
-
     private boolean isFacingWestBorder(Position position) {
       return position.getX() <= 0;
-    }
-
-    private boolean isFacingWest(Position position) {
-      return position.getOrientation() == W;
-    }
-
-    private ImmutablePosition goForwardUp(Position position) {
-      return new ImmutablePosition(position.getX(), position.getY() + 1, position.getOrientation());
     }
 
     private boolean isFacingNorthBorder(Position position, Environment environment) {
       return position.getY() >= environment.getYLimit();
     }
 
-    private boolean isFacingNorth(Position position) {
-      return position.getOrientation() == N;
-    }
   }, G {
     @Override
     public ImmutablePosition execute(ImmutablePosition currentPosition, Environment environment) {
--- src/main/java/com/example/mower/Position.java
+++ src/main/java/com/example/mower/Position.java
@@ -1,7 +1,25 @@
 package com.example.mower;
 
+import static com.example.mower.Orientation.*;
+
 public interface Position {
   int getX();
   int getY();
   Orientation getOrientation();
+
+  default boolean isFacingNorth() {
+    return getOrientation() == N;
+  }
+
+  default boolean isFacingSouth() {
+    return getOrientation() == S;
+  }
+
+  default boolean isFacingEast() {
+    return getOrientation() == E;
+  }
+
+  default boolean isFacingWest() {
+    return getOrientation() == W;
+  }
 }
